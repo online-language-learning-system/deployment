44ef2a2f4b7be801f19e6f7cfcf2bacb5db9c8c5
phccnguyenn <phccnguyenn2554@gmail.com>
2025-09-23 20:15:39 +0700

    Update Database

--- docker/mssql_scripts/run-initialization.sh
+++ docker/mssql_scripts/run-initialization.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-
 # Script Bash (Linux/Bash)
 
 set -e   # Stop script when error happens
 
 SERVER="mssql,1433"
 USER="sa"
-PASSWORD="YourStrong(!)Password"
+PASSWORD='6$3S6zPKpJ+Y2-*J'
 DB="master"
 
 echo "LOG: Waiting for SQL Server running..."
 
-#Notepad++:
-#Mở run-initialization.sh
-#Menu Edit → EOL Conversion → UNIX (LF)
-#Lưu file
+# Open run-initialization.sh with Notepad++
+# Menu Edit → EOL Conversion → UNIX (LF)
+# Save file
 
 until /opt/mssql-tools/bin/sqlcmd -S $SERVER -U $USER -P "$PASSWORD" -d $DB -Q "SELECT 1" > /dev/null 2>&1
 do
